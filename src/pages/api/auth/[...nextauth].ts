92837b81d36106818c5160c52b552b7bf40d0552
RSiby <86668225+RivaldiSiby@users.noreply.github.com>
2023-10-02 01:35:45 +0800

    fix auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,34 @@
+import { auth } from "@/firebase/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: any = {
+  pages: { login: "/login" },
+
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials: any): Promise<any> {
+        return signInWithEmailAndPassword(
+          auth,
+          credentials.email,
+          credentials.password
+        )
+          .then((userCredential) => {
+            if (userCredential.user) return userCredential.user;
+            console.log("err auth");
+            return null;
+          })
+          .catch((err) => {
+            console.log(err);
+            return null;
+          });
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
